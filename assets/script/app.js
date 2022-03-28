917c91ebb40de992738d6ea65af7222ccfe8d229
Sultan <sulaoff20@mail.ru>
2022-03-28 20:05:27 +0600

    first project

---
+++ assets/script/app.js
@@ -0,0 +1,16 @@
+'use strict'
+const numberOffilms =prompt('Сколько фильмов посмотрел?')
+const personalMovieDB = {
+    count: numberOffilms,
+    movies:{},
+    actor:{},
+    genres:[],
+    privet:false
+}
+const a = prompt('Один из просмотреных фильмов?',''),
+      b = prompt('На сколько вы оцените его?','')
+      c = prompt('Один из просмотреных фильмов?',''),
+      d = prompt('На сколько вы оцените его?','');
+personalMovieDB.movies
+
+
